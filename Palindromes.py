4c69f08a12ac4fb3906ab6427cf5155d2c21f783
An-ThRaX <ciprianionutz93@gmail.com>
2023-02-09 21:57:12 +0100

    new day

--- Palindromes.py
+++ Palindromes.py
@@ -14,3 +14,18 @@ if is_palindrome(text):
     print("It's a palindrome")
 else:
     print("It's not a palindrome")
+
+
+# palindromes 2nd attempt - transposition the text into a new list and check if they are equal
+# text = input('Please insert your palindrome: ').lower()
+# while len(text) == 0:
+#     text = input('Please insert a valid palindrome: ').lower()
+# text = "".join(text.split())
+# text = list(text)
+# text2 = []
+# for i in range(len(text)):
+#     text2.append(text[len(text) - i - 1])
+# if text == text2:
+#     print("It's a palindrome")
+# else:
+#     print("It's not a palindrome")
